a491f59b023693a1e08b22f43744b469076ce9a2
Berkay Öztürk <73599896+berkayozturkk@users.noreply.github.com>
2026-09-24 17:48:07 +0300

    Pydantic Router

---
+++ graph/chains/router.py
@@ -0,0 +1,38 @@
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.pydantic_v1 import BaseModel,Field
+from typing import Literal
+
+load_dotenv()
+
+class RouteQuery(BaseModel):
+    """
+        Route a user query to the most relevant datasource
+    """
+
+    datasource:Literal["vectorstore","websearch"] = Field(
+        ...,
+        description="Given a user question choose to route it to web search or a vectorstore",
+    )
+
+llm = ChatGoogleGenerativeAI(model="gemini-3.6-flash", temperature=0.0)
+structured_llm_router = llm.with_structured_output(RouteQuery)
+
+system = """You are an expert at routing a user question to a vectorstore or web search.
+The vectorstore contains documents related to agents, prompt engineering, and adversarial attacks.
+Use the vectorstore for questions on these topics. For all else, use websearch."""
+
+route_promt = ChatPromptTemplate.from_messages(
+    [
+        ("system" , system),
+        ("human" , "{question}"),
+    ]
+)
+
+question_router = route_promt | structured_llm_router
+
+if __name__ == '__main__':
+    print(question_router.invoke(
+        {"question":"what is the memory"}
+    ))
