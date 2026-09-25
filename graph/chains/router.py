98393977edf6cc7415bd7fe0525b9b8a681ffb38
Berkay Öztürk <73599896+berkayozturkk@users.noreply.github.com>
2026-09-25 21:24:03 +0300

    graph function

--- graph/chains/router.py
+++ graph/chains/router.py
@@ -19,7 +19,7 @@ class RouteQuery(BaseModel):
 llm = ChatGoogleGenerativeAI(model="gemini-3.6-flash", temperature=0.0)
 structured_llm_router = llm.with_structured_output(RouteQuery)
 
-system = """You are an expert at routing a user question to a vectorstore or web search.
+system = """You are an expert at routing a user question to a vectorstore or websearch.
 The vectorstore contains documents related to agents, prompt engineering, and adversarial attacks.
 Use the vectorstore for questions on these topics. For all else, use websearch."""
 
